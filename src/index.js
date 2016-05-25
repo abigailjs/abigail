1f0d0b62f1d0027388cd4d0b4183febad01989a7
59naga <i59naga@icloud.com>
2016-05-25 19:34:33 +0900

    chore: tweaks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import 'source-map-support/register';
-import npmPath from 'npm-path';
-
-// update process.env
-npmPath();
 
 export { default as Abigail } from './Abigail';
 export cli from './cli';
